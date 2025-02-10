30582744eff5f3c406682a717698f4e6c2f6dcf4
Rohit Sanap <rohitsanap177@gmail.com>
2025-02-10 21:55:37 +0530

    Introduction

---
+++ Introduction/Introduction.java
@@ -0,0 +1,13 @@
+class Introduction
+{
+  public static void main(String[]args)
+{
+      System.out.println("Hii,I am Rohit Chandrakant Sanap.");
+      System.out.println("I am from Nashik.");
+      System.out.println("Currently pursuing Bachelor's degree in Information Technology.");
+      System.out.println("From Sir Visvesvaraya Institute of Technology,Nashik.");
+      System.out.println("I like to play cricket and listen music.");
+      System.out.println("I have keen interest in learning Java.");
+      System.out.println("Thank You...");
+}
+}
