a5b16ae589bd259be27be949e52e754456aa6687
Rohit Sanap <rohitsanap177@gmail.com>
2025-04-01 11:42:54 +0530

    Patterns added

---
+++ Patterns/Alphabet/AlphabetApattern.java
@@ -0,0 +1,24 @@
+class AlphabetApattern 
+{
+	public static void main(String[] args) 
+	{
+		int n = 12;
+
+		for (int i = 0;i<n ;i++ )
+		{
+			for (int j = 0;j<n ;j++ )
+			{
+				if (j==0||i==0||j==n-1||i==(n/2))
+				{
+					System.out.print("*"+" ");
+				}
+				else 
+				{
+					System.out.print(" "+" ");
+				}
+				
+			}
+			System.out.println();
+		}
+	}
+}
