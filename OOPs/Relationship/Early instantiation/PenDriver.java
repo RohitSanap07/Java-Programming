c702d8378b7cf0e930acdde1f1c7046827611bad
Rohit Sanap <rohitsanap177@gmail.com>
2025-04-11 00:21:37 +0530

    Driver class created

---
+++ OOPs/Relationship/Early instantiation/PenDriver.java
@@ -0,0 +1,15 @@
+package Encapsulation;
+
+public class PenDriver {
+
+	public static void main(String[] args) {
+		
+		Pen p1 = new Pen("Cello", 10, "Ball pen", "Blue", 0.5,"Camel","Blue", 0.5,"Ball point");
+		
+		p1.displayPen();
+		
+		
+
+	}
+
+}
