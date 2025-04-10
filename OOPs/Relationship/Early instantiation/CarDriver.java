acb478152729cf0cc89ea4ad37c6d9d793c9d174
Rohit Sanap <rohitsanap177@gmail.com>
2025-04-11 02:16:29 +0530

    Driver class added

---
+++ OOPs/Relationship/Early instantiation/CarDriver.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class CarDriver {
+
+	public static void main(String[] args) {
+		
+	 Car c1 = new Car("Swift", 750000.0, 37, "Manual", "V6", 250, "Petrol");
+	 
+	 c1.displayCar();
+		
+	
+	}
+
+}
