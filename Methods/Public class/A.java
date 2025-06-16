d9847df4c310db803130b75aceb1673998c1ea55
Rohit Sanap <rohitsanap177@gmail.com>
2025-06-16 23:44:50 +0530

    Public class added

---
+++ Methods/Public class/A.java
@@ -0,0 +1,26 @@
+public class A 
+{
+	// In every file there is need to make one public class
+   // which having same name as file name so we can add multiple class also in one file.
+	public static void main(String[] args) 
+	{
+		System.out.println("From class A");
+	}
+
+}
+
+class B
+{
+    public static void main(String[]args)
+	{
+		System.out.println("From class B");
+	}
+}
+
+class C
+{
+	public static void main(String [] args)
+	{
+		System.out.println("From class C");
+	}
+}
